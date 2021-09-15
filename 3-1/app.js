3529cfa18cc2f6779493bacc60f3278ec6a54222
yuri0427a <yuri0427ita@gmail.com>
2021-09-16 01:17:21 +0900

    [Stadied]3-2

--- 3-1/app.js
+++ 3-1/app.js
@@ -1,38 +1,11 @@
-Vue.component('list-item', {
-    template: '<h2>フルーツ一覧</h2>'
-})
+
 
 Vue.component('fruits-list-title'), {
      template:'<h1>フルーツ一覧</h1>'
 }
 
-Vue.component('fruits-list-description', {
-    template:'<p>季節の代表的なフルーツに一覧です</p>'
+Vue.component('fruits-list', {
+    template: '<div><fruits-list-title></fruits-list-title></div>'
 })
 
-Vue.component('fruits-list-table', {
-    templete:
-    <table>
-        <tr>
-            <th>季節</th>
-            <th>フルーツ</th>   
-        </tr>
-        <tr>
-            <th>春</th>
-            <th>いちご</th>   
-        </tr>
-        <tr>
-            <th>夏</th>
-            <th>スイカ</th>   
-        </tr>
-        <tr>
-            <th>秋</th>
-            <th>ぶどう</th>   
-        </tr>
-        <tr>
-            <th>冬</th>
-            <th>みかん</th>   
-        </tr>      
-    </table>
-})
-new Vue({ el: '#fruits-list'})
+new Vue({ el: '#main'})
